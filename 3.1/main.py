9c0ee3fecba43c270aabd2bcd31dbb29ccbd7a6d
Eetu Sipilä <eetu.a.sipila@helsinki.fi>
2025-01-24 23:50:14 +0200

    Update main.py
    Selvennyksiä ja englanniksia

--- 3.1/main.py
+++ 3.1/main.py
@@ -2,43 +2,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.optimize as opt
 import pandas as pd
-#from lasku import avgVarErr
-class PaskaPituus(Exception):
+class WrongL(Exception):
     def __init__(self, message, errors):
         super().__init__(message)
         self.errors = errors
-def avgVarErr(arvot, acc):
+def avgVarErr(values, acc):
     # Laskee jokaisen mittauksen arvon keskiarvon.
     # Ottaa argumentiksi arvot, jossa on ties kuinka monen mittauksen edestä listoja.
     # Esim. [[1,2,3], [4,5,6],[7,8,9]]
     # Jokaisen jäsenen on oltava yhtä pitkiä (muuten jollekin mittaukselle ei voi laskea keskiarvoa).
-    l = len(arvot[0])
-    p = len(arvot) # Mittausten määrä
+    l = len(values[0])
+    p = len(values) # Mittausten määrä
     # Check
-    for i in arvot:
+    for i in values:
         if len(i) != l:
-            raise PaskaPituus(f"Listan pituus on epähyvä: {i}")
+            raise WrongL(f"Listan pituus on epähyvä: {i}")
         else:
             pass
     # Kuinka monta iteraatiota
-    lista = []
+    ret = []
     # i indeksi
     # Flipperiino
     for i in range(l):
         items = []
-        for k in arvot:
+        for k in values:
             items.append(k[i].item())
-        lista.append(items)
+        ret.append(items)
     mean = []
     var = []
     err = []
-    for i, arvo in enumerate(lista):
+    for i, arvo in enumerate(ret):
         mean.append(np.round(np.mean(arvo), acc).item())
         var.append(np.round(np.var(arvo), acc).item())
         err.append(np.round(np.sqrt(var[i])/np.sqrt(p), acc).item())
     return mean, var, err
 
-nayta = True
+show = True
 data = np.loadtxt('potentiaali.dat')
 print(f"Mittaukset: \n\n{pd.DataFrame(data, columns=['Etäisyys', 'Mittaus1', 'Mittaus2', 'Mittaus3'])}\n")
 
@@ -51,14 +50,14 @@ err = laskut[2]  # Virhe jokaiselle keskiarvolle (kait)
 dis = data[:,0]
 def malli(x,a):
     # Delta V = E*s
-    return a*x
+    return e*x
 sov, cov = opt.curve_fit(malli, dis, avg, sigma=err)
 sovfuc = sov[0]*dis
 
 # Lasketaan virherajat
-a_err = np.sqrt(cov[0][0])
-sovfuc_min = (sov[0] - a_err) * dis
-sovfuc_max = (sov[0] + a_err) * dis
+e_err = np.sqrt(cov[0][0])
+sovfuc_min = (sov[0] - e_err) * dis
+sovfuc_max = (sov[0] + e_err) * dis
 
 print(f"""Lasketut arvot: \n\n{pd.DataFrame(list(zip(dis,avg,err,sovfuc)), columns=['Etäisyys', 'Keskiarvo', 'Virhe', 'Sovitus'])}\n
 Sovitus:     a*s (a={sov[0]})
@@ -73,17 +72,17 @@ plt.ylim([0,10])  # Jännitelähteen jännite
 # Sovitus
 plt.plot(dis, sovfuc, label='Sovitus')
 
-# Error bounds
+# Virheraja-kuvaajat
 plt.plot(dis, sovfuc_min, 'r--', label='Virhe minimi')
 plt.plot(dis, sovfuc_max, 'g--', label='Virhe maksimi')
 
-# Mittauspisteiden keskiarvo ja virhepalkit
+# Mittauspisteet
 plt.errorbar(dis, avg, yerr=err, fmt='.', label='Mittauspisteet')
 plt.grid(True)
 plt.xlabel('Etäisyys (cm)')
 plt.ylabel('Jännite (V)')
 plt.legend()
 
-if nayta:
+if show:
     plt.show()
     fig.savefig("potentiaali.png")
